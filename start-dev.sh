5a8b9d76e36dc262d39137216f9755734da16ec5
epkboan <epkboan@gmail.com>
2013-07-27 10:27:50 +0200

    Removed -noshell

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # NOTE: mustache templates need \ because they are not awesome.
-exec erl -noshell -pa /home/pi/wpi/ebin/ /home/pi/pc/erlang/pc/ebin/ \
+exec erl -pa /home/pi/wpi/ebin/ /home/pi/pc/erlang/pc/ebin/ \
     /home/pi/Emysql/ebin ebin edit deps/*/ebin -boot start_sasl \
     -sname pc \
     -s greeting \
